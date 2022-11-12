45286e896163732f60f0e52e47c6375ea99e154d
Tricky Licky <wangombevictor5@gmail.com>
2022-11-13 00:51:57 +0300

    complete assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,19 +1,20 @@
-require "pry"
 class Triangle
-
-  attr_accessor :side_a,:side_b,:side_c
-
-  def initialize(side_a,side_b,side_c)
-    @side_a = side_a
-    @side_b = side_b
-    @side_c = side_c
+  # write code here
+  attr_accessor  :equilateral, :isosceles, :scalene
+  attr_reader :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
+  
   def kind
-    if @side_a == @side_b && @side_b == @side_c
+    validate_triangle
+    if @side1 == @side2 && @side2 == @side3
       :equilateral
-    elsif @side_a == @side_b && @side_b !=  @side_c || @side_b == @side_c && @side_b !=  @side_a || @side_a == @side_c && @side_b !=  @side_c
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
       :isosceles
-    else 
+    else
       :scalene
     end
   end
